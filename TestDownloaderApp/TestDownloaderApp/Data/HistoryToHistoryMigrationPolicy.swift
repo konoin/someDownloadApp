44bc0a05c8ed86a66a8ec3fdee4a4dde7691072c
Mikita Palyka <mikita.palyka@gmail.com>
2024-06-12 12:59:07 +0300

    CoreData Migration
    Fixed migration policy class.

--- TestDownloaderApp/TestDownloaderApp/Data/HistoryToHistoryMigrationPolicy.swift
+++ TestDownloaderApp/TestDownloaderApp/Data/HistoryToHistoryMigrationPolicy.swift
@@ -8,48 +8,18 @@
 import Foundation
 import CoreData
 
-class HistoryToHistoryMigrationPolicy: NSEntityMigrationPolicy {
-
+final class HistoryToHistoryMigrationPolicy: NSEntityMigrationPolicy {
+    
     override func createDestinationInstances(forSource sInstance: NSManagedObject, in mapping: NSEntityMapping, manager: NSMigrationManager) throws {
+        try super.createDestinationInstances(forSource: sInstance, in: mapping, manager: manager)
         
-        // Создаем новую сущность History
-        guard let historyEntityDescription = NSEntityDescription.entity(forEntityName: "History", in: manager.destinationContext) else {
-            return
-        }
+        guard let dInstance = manager.destinationInstances(forEntityMappingName: mapping.name, sourceInstances: [sInstance]).first else { return }
         
-        let newHistoryInstance = NSManagedObject(entity: historyEntityDescription, insertInto: manager.destinationContext)
-        
-        // Копируем атрибуты из старой версии в новую
-        if let sourceDate = sInstance.value(forKey: "date") as? Date {
-            newHistoryInstance.setValue(sourceDate, forKey: "date")
-        }
-        
-        if let sourceDownloaded = sInstance.value(forKey: "downloaded") as? Bool {
-            newHistoryInstance.setValue(sourceDownloaded, forKey: "downloaded")
-        }
-        
-        if let sourceId = sInstance.value(forKey: "id") as? Int64 {
-            newHistoryInstance.setValue(sourceId, forKey: "id")
-        }
-        
-        if let sourceTitle = sInstance.value(forKey: "title") as? String {
-            newHistoryInstance.setValue(sourceTitle, forKey: "title")
-        }
-
-        // Создаем новую сущность EpisodeFileURL и связываем с History
         if let sourceFileURL = sInstance.value(forKey: "fileURL") as? String {
-            guard let episodeFileURLEntityDescription = NSEntityDescription.entity(forEntityName: "EpisodeFileURL", in: manager.destinationContext) else {
-                return
-            }
-            
+            guard let episodeFileURLEntityDescription = NSEntityDescription.entity(forEntityName: "EpisodeFileURL", in: manager.destinationContext) else { return }
             let newEpisodeFileURLInstance = NSManagedObject(entity: episodeFileURLEntityDescription, insertInto: manager.destinationContext)
             newEpisodeFileURLInstance.setValue(sourceFileURL, forKey: "fileURL")
-            
-            // Устанавливаем связь между History и EpisodeFileURL
-            newEpisodeFileURLInstance.setValue(newHistoryInstance, forKey: "history")
+            newEpisodeFileURLInstance.setValue(sourceFileURL, forKey: "history")
         }
-
-        // Устанавливаем связь между старым и новым объектом
-        manager.associate(sourceInstance: sInstance, withDestinationInstance: newHistoryInstance, for: mapping)
     }
 }
